a862cf8507e8d1e956699471a530a16eaf4c0a93
Ofrani Diaz Vizcay <121466972+OfraniDV@users.noreply.github.com>
2023-08-19 16:24:18 -0700

    Resumen Total

--- commands/resumentotal.js
+++ commands/resumentotal.js
@@ -12,7 +12,7 @@ const obtenerTablas = async () => {
     const result = await pool.query(query);
     return result.rows
       .map(row => row.tablename)
-      .filter(tablename => tablename !== 'usuarios'); // Filtrar la tabla 'usuarios'
+      .filter(tablename => tablename !== 'usuarios');
   } catch (err) {
     console.error(err);
     return [];
@@ -36,6 +36,9 @@ const generarResumenCuenta = async (cuenta) => {
     let fechaAnterior = null;
     let subTotal = 0;
     let totalMes = 0;
+    let totalCreditoMes = 0;  // Nuevo: total de crédito del mes
+    let totalDebitoMes = 0;   // Nuevo: total de débito del mes
+
     const resumenText = `Resumen de la cuenta "${escapeMarkdown(cuenta)}"\n\n${
       result.rows.map((row, index) => {
         const { id, descripcion, debito, credito, total, fecha } = row;
@@ -51,7 +54,7 @@ const generarResumenCuenta = async (cuenta) => {
           transactionText += `Fecha: ${fecha.getDate()}/${fecha.getMonth() + 1}/${fecha.getFullYear()}\n`;
         }
 
-        if(descripcion === 'Saldo inicial') {
+        if (descripcion === 'Saldo inicial') {
           transactionText += `${id}. ${escapeMarkdown(descripcion)}: $${parseFloat(debito).toFixed(2)}\n`;
         } else {
           transactionText += `${id}. ${escapeMarkdown(descripcion)}: `;
@@ -60,9 +63,11 @@ const generarResumenCuenta = async (cuenta) => {
         if (debito && debito !== '0.00' && descripcion !== 'Saldo inicial') {
           transactionText += `(-) $${parseFloat(debito).toFixed(2)}\n`;
           subTotal -= parseFloat(debito);
+          totalDebitoMes += parseFloat(debito); // Sumamos al total de débito del mes
         } else if (credito && credito !== '0.00') {
           transactionText += `(+) $${parseFloat(credito).toFixed(2)}\n`;
           subTotal += parseFloat(credito);
+          totalCreditoMes += parseFloat(credito); // Sumamos al total de crédito del mes
         }
 
         if (index === result.rows.length - 1) {
@@ -75,13 +80,13 @@ const generarResumenCuenta = async (cuenta) => {
       }).join('')
     }`;
 
-    return `${resumenText}\nSaldo final del mes: $${totalMes.toFixed(2)}\n\n`;
+    return `${resumenText}\nSaldo final del mes: $${totalMes.toFixed(2)}\nTotal débito del mes: $${totalDebitoMes.toFixed(2)}\nTotal crédito del mes: $${totalCreditoMes.toFixed(2)}\n\n`;
+
   } catch (err) {
     console.error(err);
     return `⚠️ Hubo un error al generar el resumen para la cuenta "${escapeMarkdown(cuenta)}". Por favor, verifica que la cuenta exista y que haya transacciones registradas.\n\n`;
   }
 };
-
 
 const resumenTotal = async (ctx) => {
   const tablas = await obtenerTablas();
@@ -98,7 +103,6 @@ const resumenTotal = async (ctx) => {
     resumenCompleto += resumen;
   }
 
-  // Divide el mensaje en varios mensajes si excede el límite de caracteres de Telegram
   const limiteCaracteres = 4096;
   const cantidadMensajes = Math.ceil(resumenCompleto.length / limiteCaracteres);
 
